571b0aba208081048373d017eab0e4de7474c52c
Justin <web@jstn.ly>
2025-07-01 21:05:44 +0700

    Update TypeScript and ESLint versions across the project
    - Bumped TypeScript version from 5.8.2 to 5.8.3 in multiple package.json files for improved features and bug fixes.
    - Updated ESLint version from 9.29.0 to 9.30.0 to incorporate the latest linting rules and enhancements.
    - Introduced a new builder package with its own configuration and scripts for better modularity.
    - Adjusted import paths in the API schema to reference the new builder package.
    - Updated package.json files to include the new builder package as a dependency in relevant applications.

--- packages/database/src/pothos.config.js
+++ packages/database/src/pothos.config.js
@@ -1,17 +1,22 @@
 /** @type {import('prisma-generator-pothos-codegen').Config} */
+
+const generatedGqlDir = "./generated/graphql"
+
 export const inputs = {
-  outputFilePath: "./generated/graphql/pothos-inputs.ts",
-  prismaImporter: `import { Prisma } from "../../src";`,
+  outputFilePath: `${generatedGqlDir}/pothos-inputs.ts`,
+  prismaImporter: 'import { Prisma } from "@prisma/client";',
 }
+
 export const crud = {
-  outputDir: "./generated/graphql/pothos-inputs",
-  inputsImporter: `import * as Inputs from '../../../../generated/graphql/pothos-inputs';`,
-  resolverImports: `\nimport { db } from "../../../../../src";`,
-  prismaImporter: `import { Prisma } from "../../../src";`,
+  outputDir: `${generatedGqlDir}/pothos-inputs`,
+  inputsImporter: "import * as Inputs from '../pothos-inputs';",
+  resolverImports: '\nimport { db } from "../../../src/client";',
+  prismaImporter: 'import { Prisma } from "@prisma/client";',
   prismaCaller: "db",
+  builderLocation: "@repo/builder",
 }
+
 export const global = {
-  builderLocation: "../../apps/api/src/builder",
   replacer: (generated) => {
     return `/* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck

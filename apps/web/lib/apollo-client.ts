d7a7b2168faaaf27f04ba471b71884e8d38ba706
Justin <web@jstn.ly>
2025-07-17 20:38:03 +0700

    Update GraphQL endpoint in Apollo client configuration for local development

--- apps/web/lib/apollo-client.ts
+++ apps/web/lib/apollo-client.ts
@@ -5,7 +5,7 @@ import {
   InMemoryCache,
 } from "@apollo/client-integration-nextjs"
 
-const uri = process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://ex:4000/graphql"
+const uri = process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://localhost:4000/graphql"
 
 export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
   return new ApolloClient({

fbc69d7d28bf41ec63d89900729a6ee4da3fef02
Justin <web@jstn.ly>
2025-07-19 02:11:57 +0700

    Enhance Header component with navigation links and update styles
    - Added a navigation menu to the Header component, including links for Servers, Games, Players, Login, and Register.
    - Updated the layout of the Header for improved spacing and alignment.
    - Modified dark theme CSS variables for primary, accent, and input styles to enhance visual consistency.
    - Removed unnecessary whitespace in the button component file.

--- apps/web/features/common/components/header.tsx
+++ apps/web/features/common/components/header.tsx
@@ -1,15 +1,60 @@
+import { Button } from "@repo/ui/button"
 import { cn } from "@repo/ui/lib/utils"
+import Link from "next/link"
 
 interface HeaderProps {
   fixed?: boolean
 }
 
+const navItems = [
+  {
+    label: "Servers",
+    href: "/servers",
+  },
+  {
+    label: "Games",
+    href: "/games",
+  },
+  {
+    label: "Players",
+    href: "/players",
+  },
+  {
+    label: "Login",
+    href: "/login",
+  },
+  {
+    label: "Register",
+    href: "/register",
+    type: "button",
+  },
+]
+
 export function Header({ fixed = false }: HeaderProps) {
   return (
     <header className={cn("w-full", fixed && "fixed top-0 left-0 right-0 z-50")}>
-      <div className="max-w-screen-lg mx-auto py-4 font-bold">
-        {process.env.NEXT_PUBLIC_APP_NAME}{" "}
-        <span className="text-xs text-muted-foreground">v1.0.0</span>
+      <div className="max-w-screen-lg mx-auto py-5 flex justify-between items-center">
+        <div className="flex items-center gap-2 font-bold">
+          {process.env.NEXT_PUBLIC_APP_NAME}{" "}
+          <span className="text-xs text-muted-foreground">v1.0.0</span>
+        </div>
+        <nav className="flex items-center gap-5">
+          {navItems.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              className="uppercase tracking-tight text-xs font-semibold text-zinc-300 hover:text-white transition-colors"
+            >
+              {item.type === "button" ? (
+                <Button variant="outline" size="sm" className="text-xs cursor-pointer">
+                  {item.label}
+                </Button>
+              ) : (
+                item.label
+              )}
+            </Link>
+          ))}
+        </nav>
       </div>
     </header>
   )

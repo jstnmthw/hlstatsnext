44100b321a6dea08198ee4a6333556e2dbe1ee10
Justin <web@jstn.ly>
2025-08-03 18:16:24 +0700

    Move web app to src folder to match our other software

--- apps/web/codegen.ts
+++ apps/web/codegen.ts
@@ -2,9 +2,9 @@ import { CodegenConfig } from "@graphql-codegen/cli"
 
 const config: CodegenConfig = {
   schema: "http://localhost:4000/graphql",
-  documents: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}", "features/**/*.{ts,tsx}"],
+  documents: ["src/**/*.{ts,tsx}"],
   generates: {
-    "./lib/gql/": {
+    "./src/lib/gql/": {
       preset: "client",
       plugins: [],
     },
